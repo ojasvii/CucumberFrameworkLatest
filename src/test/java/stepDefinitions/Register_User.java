2f5cb1bf72359daf046fc9555fe2e444252438b3
Ojasvii Pratap Singh <ojasvi.pratap17@gmail.com>
2024-11-11 22:22:01 +0530

    changes in the register user steps file

--- src/test/java/stepDefinitions/Register_User.java
+++ src/test/java/stepDefinitions/Register_User.java
@@ -1,13 +1,16 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.And;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import pageObjects.Base_PO;
+
+import java.util.List;
 
 public class Register_User extends Base_PO {
 
@@ -28,64 +31,112 @@ public class Register_User extends Base_PO {
         System.out.println(homeText);
     }
     @When("the user clicks on {string} button")
-    public void the_user_clicks_on_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_user_clicks_on_button(String signupLogin) {
+        WebElement signup = getDriver().findElement(By.xpath("//header[@id='header']/div/div/div/div[2]/div/ul/li[4]"));
+       signup.click();
+
     }
     @Then("{string} should be visible")
-    public void should_be_visible(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void should_be_visible(String newUserText) {
+        WebElement newUserSignUp = getDriver().findElement(By.xpath("//div[@class='signup-form']/h2"));
+        String actualNewUserSignUp = newUserSignUp.getText().trim();  // Trim whitespace
+        if (actualNewUserSignUp.contains("!")) {
+            actualNewUserSignUp = actualNewUserSignUp.substring(0, actualNewUserSignUp.indexOf('!') + 1); // Extract up to '!'
+        }
+        System.out.println("NewuserSignUp: " + actualNewUserSignUp);
+        Assert.assertEquals(actualNewUserSignUp, newUserText);
     }
     @When("Enter name and email address")
     public void enter_name_and_email_address() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement name = getDriver().findElement(By.xpath("//input[@name='name']"));
+        name.sendKeys("ojasvii");
+
+
+        WebElement email = getDriver().findElement(By.xpath("//div[@class='signup-form']//input[@name='email']"));
+        email.sendKeys("sdet@team.com");
+
     }
     @When("Click signup {string} button")
     public void click_signup_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement button = getDriver().findElement(By.xpath("//div[@class='signup-form']//button"));
+        button.click();
     }
-    @Then("Verify that {string} should be visible")
-    public void verify_that_should_be_visible(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("Verify that {} should be visible")
+    public void verify_that_should_be_visible(String information) {
+        WebElement accountInformationForm = getDriver().findElement(By.xpath("(//div[@class='login-form']//b)[1]"));
+        String  aif  = accountInformationForm.getText();
+        Assert.assertEquals(aif,information);
     }
+
+
     @When("Fill details: Title, Name, Email, Password, Date of birth")
     public void fill_details_title_name_email_password_date_of_birth() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement title = getDriver().findElement(By.xpath("//div[@id='uniform-id_gender1']"));
+        title.click();
+
+        WebElement name = getDriver().findElement(By.xpath("//input[@id='name']"));
+        if(name.isEnabled()){
+            name.clear();
+            name.sendKeys("ojasvii");
+        }
+
+
+
+        WebElement email = getDriver().findElement(By.xpath("//input[@id='email']"));
+        if(email.isEnabled()){
+            email.clear();
+            email.sendKeys("sdet@team.com");
+        }
+
+
+        WebElement passwrod = getDriver().findElement(By.xpath("//input[@id='password']"));
+        passwrod.clear();
+        passwrod.sendKeys("sdet@team.com");
+
+        WebElement days = getDriver().findElement(By.xpath("//select[@id='days']"));
+
+        Select select = new Select(days);
+        select.selectByIndex(18);
+
+        WebElement months = getDriver().findElement(By.xpath("//select[@id='months']"));
+
+        Select select2 = new Select(months);
+        select2.selectByIndex(1);
+
+        WebElement year = getDriver().findElement(By.xpath("//select[@id='uniform-years']"));
+
+        Select select3 = new Select(year);
+        select3.selectByIndex(18);
+
     }
     @When("Select checkbox {string}")
     public void select_checkbox(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement newLetter = getDriver().findElement(By.xpath("//input[@id='newsletter']"));
+        newLetter.click();
+
+       WebElement offer = getDriver().findElement(By.xpath("//input[@id='optin']"));
+       offer.click();
     }
+
     @When("Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number")
     public void fill_details_first_name_last_name_company_address_address2_country_state_city_zipcode_mobile_number() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("Click {string}")
     public void click(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("Click on {string} button after account created")
     public void click_on_button_after_account_created(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("Click delete {string} button")
     public void click_delete_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("click continue {string} button after account deleted")
     public void click_continue_button_after_account_deleted(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
